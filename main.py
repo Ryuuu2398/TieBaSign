44471ece50682b9668927f95d3103f92ce56df0f
Ryuuu2398 <38608602+Ryuuu2398@users.noreply.github.com>
2024-05-30 17:03:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def send_email(sign_list):
     
     try:
         # 建立 SMTP 、SSL 的连接，连接发送方的邮箱服务器
-        smtp.SMTP_SSL(HOST,465)
+        smtp = smtplib.SMTP_SSL(HOST,465)
 
         # 登录发送方的邮箱账号
         smtp.login(FROM, AUTH)
